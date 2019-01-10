fdfa03b8f9bff4737f07488827d7a6a73867bdcd
Russell Senior <russell@personaltelco.net>
2019-01-09 16:00:22 -0800

    sensor-sink: add incoming log file rotation on SIGHUP
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- sensor-sink.py
+++ sensor-sink.py
@@ -1,15 +1,48 @@
-#
-#   Weather update client
-#   Connects SUB socket to tcp://localhost:5556
-#   Collects weather updates and finds avg temp in zipcode
-#
-
 import sys
 import zmq
+import signal
+import io
+import os
+import os.path
 
 #  Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
+
+logfname = 'incoming.log'
+newfname = 'rotated.log'
+printed = False
+
+if os.path.isfile(logfname):
+	if os.stat(logfname).st_size > 0:
+		printed = True
+
+logfile = io.open(logfname,'a')
+
+closeFlag = False
+printing = False
+
+def rotateLog():
+	global printed
+	global logfile
+	global closeFlag
+
+	if printed:
+		logfile.close()
+		os.rename(logfname,newfname)
+		logfile = open(logfname,"w")
+	closeFlag = False
+
+def handleHUP(signalNumber, frame):
+	global printing
+	global closeFlag
+
+	if printing:
+		closeFlag = True
+	else:
+		rotateLog()
+
+signal.signal(signal.SIGHUP, handleHUP)
 
 print("Collecting updates from sensor servers")
 socket.bind("tcp://*:5556")
@@ -19,5 +52,11 @@ socket.setsockopt(zmq.SUBSCRIBE, b'')
 # Process incoming messages
 while True:
 	string = socket.recv_string()
+	printing = True
+	logfile.write(string + "\n")
+	printed = True
 	print("received: %s" % (string))
+	printing = False
+	if closeFlag:
+		rotateLog()
 
