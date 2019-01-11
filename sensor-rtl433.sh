fe7b29f61f9450008d20bf09c82c6c7c15595b1c
Russell Senior <russell@personaltelco.net>
2019-01-11 01:43:39 -0800

    sensor-rtl433: correct the sid field number to 109
    I switched to using the SID for Oregon Scientific sensors instead
    of the channel because I thought it would be stable and more unique,
    however, it appears that the SID changes on each reset. That kind
    of sucks, but maybe the best for now.
    
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- sensor-rtl433.sh
+++ sensor-rtl433.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-stdbuf -o L /home/russell/src/rtl_433/src/rtl_433 -F csv | awk -W interactive '$0 != last { print $0 ; last = $0 }' | awk -W interactive -F, '$4 ~ /Acurite tower sensor/ { print $1,"acu-" $6 "-temp",$9 ; print $1,"acu-" $6 "-rh",$12 } $4 ~ /OSv1 Temperature Sensor/ { print $1,"os-" $121 "-temp",$9 }' | python /home/russell/src/sensorsys/sensor-pub.py dodson
+stdbuf -o L /home/russell/src/rtl_433/src/rtl_433 -F csv | awk -W interactive '$0 != last { print $0 ; last = $0 }' | awk -W interactive -F, '$4 ~ /Acurite tower sensor/ { print $1,"acu-" $6 "-temp",$9 ; print $1,"acu-" $6 "-rh",$12 } $4 ~ /OSv1 Temperature Sensor/ { print $1,"os-" $109 "-temp",$9 }' | awk -W interactive 'NF == 3' | python /home/russell/src/sensorsys/sensor-pub.py dodson
