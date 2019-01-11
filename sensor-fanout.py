08a917be14227490ca386f30563a554f9c618b54
Russell Senior <russell@klickitat.com>
2019-01-10 22:01:22 -0800

    sensor-fanout: distribute records to daily files in by-sensor directories
    Signed-off-by: Russell Senior <russell@klickitat.com>

---
+++ sensor-fanout.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+topdir = "data"
+f = { }
+
+if not os.path.isdir(topdir):
+	os.mkdir(topdir)
+
+for line in sys.stdin:
+	(time,sensor,rest) = line.rstrip().split(" ")
+	# print(sensor,time,rest)
+	sensorPath = topdir + "/" + sensor
+	if not os.path.isdir(sensorPath):
+		os.mkdir(sensorPath)
+	day = int(time)
+	day = day - (day % 86400)
+	fname = sensorPath + "/" + ("%u" % day)
+	out = f.get(fname)
+	if out == None:
+		out = open(fname,"a+")
+		f[fname] = out
+		# print("opened %s for appending" % fname)
+	out.write("%s %s\n" % (time,rest))
+
+for fname in f:
+	out = f[fname]
+	out.close()
+	print("closed %s" % fname)
